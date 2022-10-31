9fa5c452c38c086929e8885aedb0523fb3ba4230
Manishkip <89375969+Manishkip@users.noreply.github.com>
2022-10-31 11:20:29 +0530

    Create duplicate_no.cpp

---
+++ Arrays/duplicate_no.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i = 0; i< nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        int dup = 0;
+        for(auto it: mp){
+            if(it.second > 1){
+                dup = it.first;
+                break;
+            }
+        }
+        return dup;
+    }
+};
+
+
+//TC : O(N)
+//SC : O(N)
